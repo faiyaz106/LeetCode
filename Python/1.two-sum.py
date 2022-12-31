64dcde2b76bc63ece0d67f38e79843cde7182721
faiyaz106 <faiyazahmed106@gmail.com>
2022-12-31 18:59:07 -0500

    Create 1.two-sum.py

---
+++ Python/1.two-sum.py
@@ -0,0 +1,19 @@
+# Link: https://leetcode.com/problems/two-sum/
+# Time Complexity: O(n)  Here n=len(nums)
+# Space Complexity: O(n) 
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict={}
+        n=len(nums)
+        for i in range(n):
+            residue=target-nums[i]
+            if (residue in dict ):
+                return [i,dict[residue]]
+            else:
+                dict[nums[i]]=i
