903195d3790b7792c73c15fd1473a06dbd5c6b86
faiyaz106 <faiyazahmed106@gmail.com>
2022-12-31 19:09:32 -0500

    Create 121.best-time-to-buy-and-sell-stock.py

---
+++ Python/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Time Complexity:O(n) where n=len(prices)
+# Space Complexity:O(1)
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_=min(prices)
+        max=0
+        B_i=0
+        S_i=1
+        for i in range(len(prices)-1):
+            if (prices[S_i]>prices[B_i]):
+                if max<prices[S_i]-prices[B_i]:
+                    max=prices[S_i]-prices[B_i]
+            else:
+                B_i=S_i
+            S_i=S_i+1
+            
+    
+
+
+        return max
