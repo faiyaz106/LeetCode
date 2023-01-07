706eb9524c59bb0bf44d0d8eed27b24559fb0c0b
Faiyaz Ahmad <faiyazahmed106@gmail.com>
2023-01-07 17:15:24 -0500

    Create 53. maximum-subarray.py

---
+++ Python/53. maximum-subarray.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/maximum-subarray/description/
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum=nums[0]
+        c_sum=0
+        for i in nums:
+            if c_sum<0:
+                c_sum=0
+            c_sum+=i
+            max_sum=max(max_sum,c_sum)
+        return max_sum
