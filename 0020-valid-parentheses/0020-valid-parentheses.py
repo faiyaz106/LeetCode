d56c90b48e6b80dd080a8900ac1869f8e5be6aca
Faiyaz Ahmad <faiyazahmed106@gmail.com>
2023-01-17 12:46:07 -0500

    Time: 18 ms (79.54%), Space: 13.6 MB (35.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dict={')':'(','}':'{',']':'['}
+        stack=[]
+        for i in s:
+            if  i in dict.keys():
+                if stack and dict[i]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+
+        if len(stack)==0:
+            return True
+        else:
+            return False
+
+
+
+
+
+            
+
+
+                
