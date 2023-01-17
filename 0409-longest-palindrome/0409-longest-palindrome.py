eaa8e01eee5c02737c25150e5cc940c144029f21
Faiyaz Ahmad <faiyazahmed106@gmail.com>
2023-01-17 12:51:36 -0500

    Time: 16 ms (93.56%), Space: 13.5 MB (67.63%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dict={}
+        for i in s:
+            if i in dict:
+                dict[i]+=1
+            else:
+                dict[i]=1
+        count=0
+        odd=0
+        for i in dict.keys():
+            if dict[i]%2==0:
+                count=count+dict[i]
+            else:
+                odd=1
+                count+=(dict[i]-1)
+            
+        
+        return count+odd
